a12fd3149034986831202e8a3de9cf300903e139
Mgepahmge <yss489589139@outlook.com>
2025-04-16 19:45:07 +0800

    feat(GUI) : 为项目添加fltk库，并编写一个demo进行测试

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,38 @@
 
 #include "PacketCapture.h"
 
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+
+
+void button_callback(Fl_Widget* widget, void* data) {
+    auto* box = static_cast<Fl_Box*>(data);
+    static int count = 0;
+    count++;
+    const std::string buffer = std::to_string(count);
+    box->label(buffer.c_str());
+    box->redraw();
+}
+
 int main()
 {
+    auto* window = new Fl_Window(400, 300, "FLTK");
+
+    auto* box = new Fl_Box(100, 50, 200, 50, "FLTK");
+    box->box(FL_UP_BOX);
+    box->labelfont(FL_BOLD);
+    box->labelsize(18);
+    box->labeltype(FL_SHADOW_LABEL);
+
+    auto* button = new Fl_Button(150, 150, 100, 40, "click me");
+    button->callback(button_callback, box);
+
+    window->end();
+    window->show();
+
+    return Fl::run();
     std::cout << wcls::DeviceEnumerator::Instance() << std::endl;
     wcls::PacketCapture::Instance().SetDevice(wcls::DeviceEnumerator::Instance()[1]->name);
     wcls::PacketCapture::Instance().Initialize();
